521c6b37a8036958b06e3c7e487f1d92c18ca9df
PeterWright <peterwrighten@gmail.com>
2022-01-03 01:41:44 +0900

    update: HashTable(bug)

---
+++ MyAlgorithms/w4_ST_search/HashTable/StringHashCode.java
@@ -0,0 +1,13 @@
+public final class String {
+    private int hash = 0;
+    private final char[] s;
+    public int hashCode() {
+        int h = hash;
+        if(h != 0) return h;
+        for(int i = 0; i < length(); i++) 
+            h = s[i] + (31 * h);
+        hash = h;
+        return h;       
+    }
+    
+}

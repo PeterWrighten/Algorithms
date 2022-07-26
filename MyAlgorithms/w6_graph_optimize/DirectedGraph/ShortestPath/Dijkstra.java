c3805b076829e9dec91fd5808fb10338bcc64bdb
PeterWrighten <peterwrighten@gmail.com>
2022-07-26 13:13:15 +0900

    Shortest Path

--- MyAlgorithms/w6_graph_optimize/DirectedGraph/ShortestPath/Dijkstra.java
+++ MyAlgorithms/w6_graph_optimize/DirectedGraph/ShortestPath/Dijkstra.java
@@ -1,4 +1,17 @@
+/**********************************************
+ *  public class Dijkstra:
+ *      private DirectedGraph G;
+ *      public Dijkstra(DirectedGraph G) {
+ *          this.G = G;
+ *      }
+ * 
+ *      public int dijkstra(int s);
+ **********************************************/
+import api.WE.EdgeWeightedDigraph;
+import java.util.PriorityQueue;
 
 public class Dijkstra {
+    private EdgeWeightedDigraph G;
+    private int[] disTo;
     
 }

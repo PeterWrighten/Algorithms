ff7fbd44097987c0ed98529170b0f9e6646d3843
PeterWrighten <peterwrighten@gmail.com>
2022-10-20 14:50:05 +0900

    update: Fleury

--- StandardSolution/Graph/Fleury.cc
+++ StandardSolution/Graph/Fleury.cc
@@ -4,6 +4,34 @@
 #include <string>
 using namespace std;
 
-// Todo
+class Graph {
+        int Vertex;
+        int E;
+    public:
+        vector<int> Adj;
+        Graph(int v) {
+            Adj.resize(v);
+        }
 
+        int V() const {
+            return Vertex;
+        }
+
+        int& adj(int); 
+};
+
+int& Graph::adj(int v) {
+    return Adj[v];
+}
+
+int main() {
+    Graph g(3);
+    g.adj(2) = 1;
+
+
+    for(auto i: g.Adj) {
+        cout << i << endl;
+    }
+    return 0;
+}
 

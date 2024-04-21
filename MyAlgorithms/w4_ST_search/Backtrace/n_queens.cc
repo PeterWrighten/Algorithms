3075e13affb006141b0bcd21ce59fb72bd028fc4
PeterWrighten <peterwrighten@gmail.com>
2024-04-21 11:43:50 +0900

    feat(display) visualize n_queens

--- MyAlgorithms/w4_ST_search/Backtrace/n_queens.cc
+++ MyAlgorithms/w4_ST_search/Backtrace/n_queens.cc
@@ -7,6 +7,22 @@ std::vector<ll> x;
 
 ll n;
 
+void print(ll n) {
+    std::cout << "-----------------------" << std::endl;
+    for(ll i = 0; i < n; i++) {
+        for(ll j = 0; j < n; j++) {
+            if(x[i] == j) {
+                std::cout << "Q";
+            } else {
+                std::cout << ".";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "-----------------------" << std::endl;
+}
+
 bool canPlace(ll k) {
     for(ll i = 0; i < k; i++) {
         if(x[k] == x[i] || std::abs(x[i] - x[k]) == std::abs(i - k)) {
@@ -21,6 +37,7 @@ ll recursive_backtrace(ll k, ll n) {
     ll sum = 0;
 
     if(k == n) {
+        print(n);
         return 1;
     }
 
@@ -37,23 +54,28 @@ ll recursive_backtrace(ll k, ll n) {
 ll iter_backtrace(ll n) {
     ll sum = 0;
     ll k = 0;
-    std::vector<ll> x(n);    
+
+    for(ll i = 0; i < n; i++) {
+        x[i] = -1;
+    }
+    
     while(k > -1) {
         if(k == n) {
+            print(n);
             sum++;
             k--;
             continue;
         }
 
-        if(x[k] < n) {
+        
+        if(++x[k] < n) {
             if(canPlace(k)) {
                 k++;
-            }
+            } 
         } else {
-            x[k] = 0;
+            x[k] = -1;
             k--;
         }
-        x[k]++;
     }
 
     return sum;
@@ -63,9 +85,10 @@ int main() {
     std::cin >> n;
     x.resize(n);
 
-    std::cout << "Recursive: " << recursive_backtrace(0, n) << std::endl;
 
     std::cout << "Iterate: " << iter_backtrace(n) << std::endl;
+
+    std::cout << "Recursive: " << recursive_backtrace(0, n) << std::endl;
     
     return 0;
 }
